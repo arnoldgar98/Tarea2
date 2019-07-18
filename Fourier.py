be7ea5b137e3356c37281be8e663236d99c00179
arnolgar98 <as.fasfd>
2019-07-18 20:15:32 +0000

    otro

--- Fourier.py
+++ Fourier.py
@@ -7,20 +7,39 @@ from matplotlib.colors import LogNorm
 #almacenar las dos imagenes
 imagen1= plt.imread("cara_02_grisesMF.png")
 imagen2= plt.imread("cara_03_grisesMF.png")
-#transformada
+#transformada 
 trans2d = fft2(imagen1)
 trans2d2= fft2(imagen2)
 #muestra
-#Haga un filtro
-
+##imagenes de la transformada y las frecuencias
 plt.figure()
+plt.subplot(1,2,1)
 plt.imshow(abs(trans2d),norm=LogNorm(vmin=5))
+plt.title("transformada de fourier")
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.imshow(abs(trans2d2),norm=LogNorm(vmin=5))
 plt.title("transformada de fourier")
 plt.colorbar()
 plt.savefig("FFT2D.png")
 #frecuencias
-frecimagen=np.fft.fftshift(imagen1)
-frecimagen2=np.fft.fftshift(imagen2)
+frecimagen=np.fft.fftshift(trans2d)
+frecimagen2=np.fft.fftshift(trans2d2)
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(abs(frecimagen),norm= LogNorm())
+plt.title("transformada de fourier")
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.imshow(abs(frecimagen2),norm= LogNorm())
+plt.title("transformada de fourier")
+plt.colorbar()
+plt.savefig("frecuencias")
+
+##filtro de las frecuencias
+def gausiana():
+    
+
 
 #Inversa para poder graficar
 invtrans= ifft2(trans2d)     
